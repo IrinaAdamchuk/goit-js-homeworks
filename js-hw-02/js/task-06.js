885898de6c20097276b124987ea9e61f86ce4b43
Irina Adamchuk <i.admira@gmail.com>
2019-11-29 01:02:18 +0200

    fixed code in task-6

--- js-hw-02/js/task-06.js
+++ js-hw-02/js/task-06.js
@@ -1,33 +1,27 @@
 "use strict";
 
-let input = prompt("Введите любое число");
+let input;
 const numbers = [];
 let total = 0;
 
-if (input !== null) {
-  numbers[0] = Number(input);
+while (input !== null) {
+  input = prompt("Введите любое число");
+  const inputInNumb = Number(input);
+  console.log(inputInNumb);
+  const isNaN = Number.isNaN(inputInNumb);
 
-  while (input !== null) {
-    input = prompt("Введите любое число");
-    const inputInNumb = Number(input);
-    const isNaN = Number.isNaN(inputInNumb);
-
-    if (isNaN) {
-      alert("Было введено не число, попробуйте еще раз");
-      continue;
-    }
-    numbers.push(inputInNumb);
+  if (isNaN) {
+    alert("Было введено не число, попробуйте еще раз");
+    continue;
   }
-
-  numbers.pop();
-
-  for (const number of numbers) {
-    total += number;
-  }
-
-  console.log(numbers);
-  console.log(`Общая сумма чисел равна ${total}`);
-  alert(`Общая сумма чисел равна ${total}`);
-} else {
-  alert("Отменено пользователем");
+  numbers.push(inputInNumb);
 }
+
+numbers.pop();
+
+for (const number of numbers) {
+  total += number;
+}
+
+console.log(numbers);
+alert(`Общая сумма чисел равна ${total}`);
